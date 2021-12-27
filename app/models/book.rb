8dca8d8c3d4dcb475106d9f6f664cd65bfd68db3
HMTanzeel <mtanzeel6363@gmail.com>
2021-12-27 18:19:26 +0500

    custom message for nil/.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,8 @@
 class Book < ApplicationRecord
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: { message: 'must be given & unique'}, uniqueness: true
     validates_each :name do |record, attr, value|
     record.errors.add(attr, 'start with upper case') if value =~ /\A[[:lower:]]/
     end
     validates_with Validator
+    # validates :name, length: { is: 5 }, allow_blank: true
 end
