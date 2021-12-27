bc241f52d0a982b2ac1fa13ba63344beb9ffc237
HMTanzeel <mtanzeel6363@gmail.com>
2021-12-27 17:52:59 +0500

    latter cases condition

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
     validates :name, presence: true, uniqueness: true
+    validates_each :name do |record, attr, value|
+    record.errors.add(attr, 'start with upper case') if value =~ /\A[[:lower:]]/
+    end
     validates_with Validator
 end
