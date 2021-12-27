0fb568c0de549591b305cc45e4a5e87704edcd86
HMTanzeel <mtanzeel6363@gmail.com>
2021-12-27 18:25:35 +0500

    custom message for with value.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,9 @@
 class Book < ApplicationRecord
-    # validates :name, presence: { message: 'must be given & unique'}, uniqueness: true
+    validates :name, presence: { message: 'must be given & unique'},
+    uniqueness: { message: "%{value} seems wrong" }
     validates_each :name do |record, attr, value|
     record.errors.add(attr, 'start with upper case') if value =~ /\A[[:lower:]]/
     end
     validates_with Validator
-    validates :name, length: { is: 5 }, allow_blank: true
+    # validates :name, length: { is: 5 }, allow_blank: true
 end
